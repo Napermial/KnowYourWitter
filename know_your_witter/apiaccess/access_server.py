78009e6481118fbc5fdf01bc1a9a5699cee397bf
Hatvani-1966 <hatvaniattila@t-online.hu>
2021-03-17 11:55:12 +0100

    decorator for api access

---
+++ know_your_witter/apiaccess/access_server.py
@@ -0,0 +1,31 @@
+import twitter
+
+def connect_to_api(func):
+    def wrapper(*args, **kwargs):
+        api = twitter.Api(consumer_key='',
+                  consumer_secret='',
+                  access_token_key='',
+                  access_token_secret='')
+        func(api, *args, **kwargs)
+    return wrapper()
+
+@connect_to_api
+def get_tweets_of_user(api, user):
+    user = api.get_user(user)
+    return user.get_tweets()
+
+@connect_to_api
+def validate_username(api, username):
+    try:
+        api.get_user(username)
+        return True
+    except:
+        ValueError
+
+
+def main():
+    validate_username(username="a")
+    get_tweets_of_user(user="a")
+
+if __name__ == '__main__':
+    main()
