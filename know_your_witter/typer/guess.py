8f6a7b3c356478eb6439290061044540439894a6
Hatvani-1966 <hatvaniattila@t-online.hu>
2021-03-17 12:13:44 +0100

    generator for generating new prediction

---
+++ know_your_witter/typer/guess.py
@@ -0,0 +1,16 @@
+import tensorflow.keras as tf
+
+def structure_data()
+    pass
+
+def create_model(inputs, outputs):
+    return tf.Model(inputs=inputs, outputs=outputs)
+
+def generate_new_prediction(model):
+    yield model.predict()
+
+def main():
+
+
+if __name__ == '__main__':
+    main()
