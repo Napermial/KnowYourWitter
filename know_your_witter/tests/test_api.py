8bd105db4d9611b37d7fd3c053d0d575e9711929
Norphet <norbert.namesnyik@studentambassadors.com>
2021-03-23 20:08:49 +0100

    Username validation tests v1.8

--- know_your_witter/tests/test_api.py
+++ know_your_witter/tests/test_api.py
@@ -4,20 +4,20 @@ from unittest.mock import Mock
 from know_your_witter.apiaccess import access_server
 
 
-class TestSum(unittest.TestCase):
+class TestApi(unittest.TestCase):
     def test_get_tweets(self):
         mock_twitter = Mock()
         mock_twitter.return_value = {'username': 'testuser', 'statuses': [{'message': 'test message'}]}
         tweets = access_server.get_tweets_of_user(mock_twitter, 'testuser')
         self.assertEqual(tweets, mock_twitter.return_value)
 
-  def test_username(self):
+    def test_username(self):
         mock_user = Mock()
         mock_user.return_value = {'username': 'testuser'}
         users = access_server.validate_username(mock_user, 'testuser')
         self.assertEqual(users, mock_user.return_value)
 
-  def test_username_error(self):
+    def test_username_error(self):
         mock_user = Mock()
         mock_user.return_value = {'username': 'testuser'}
         users=access_server.validate_username(mock_user, 'testuser')
