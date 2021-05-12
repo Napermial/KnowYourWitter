0b7c69454fe333104c01a72a58f5e5a8a349edec
Napermial <h>
2021-05-12 18:35:04 +0200

    api refactored

--- know_your_witter/apiaccess/api.py
+++ know_your_witter/apiaccess/api.py
@@ -1,32 +1,13 @@
-import tweepy, os
-import datetime
+import tweepy
+import os
 
 
-# Get tweet function, keyword input, related tweets database output
-def get_tweets(keyword):
+def get_tweets(user_name):
     auth = tweepy.OAuthHandler(os.getenv("TWITTER_CONSUMER_KEY"), os.getenv("TWITTER_CONSUMER_SECRET"))
-    auth.set_access_token(os.getenv("TWITTER_ACCESS_TOKEN"), os.getenv("TWITTER_ACCESS_TOKEN_SECRET"))
     api = tweepy.API(auth, wait_on_rate_limit=True)
 
-    # Collected tweet date period
-    today = datetime.date.today()
-    yesterday = today - datetime.timedelta(days=1)
-
-    # Tweet collection
-    tweets_list = tweepy.Cursor(api.search, q=keyword + str(yesterday) + " until:" + str(today), tweet_mode='extended',
-                                lang='en').items()
-
-    # convert the raw data into a variable
+    tweets_list = api.user_timeline(screen_name=user_name, exclude_replies=True, count=100)
     output = []
     for tweet in tweets_list:
-        text = tweet._json["full_text"]
-        print(text)
-        favourite_count = tweet.favorite_count
-        retweet_count = tweet.retweet_count
-        created_at = tweet.created_at
-
-        line = {'text': text, 'favourite_count': favourite_count, 'retweet_count': retweet_count,
-                'created_at': created_at}
-        output.append(line)
-
+        output.append(tweet._json["text"])
     return output
--- know_your_witter/typer/guess.py
+++ know_your_witter/typer/guess.py
@@ -3,64 +3,91 @@ from tensorflow.keras import model as tf
 from know_your_witter.apiaccess import api
 
 
-def generate_new_prediction(model):
-    yield model.predict()
-
-
 def clean_tweet(tweet):
     tweet = re.sub("@[A-Za-z0-9]+", "", tweet)
     tweet = re.sub(r"(?:\@|http?\://|https?\://|www)\S+", "", tweet)
     tweet = " ".join(tweet.split())
     tweet = ''.join(c for c in tweet if c not in emoji.UNICODE_EMOJI)
     tweet = tweet.replace("#", "").replace("_", " ")
-    tweet = " ".join(w for w in nltk.wordpunct_tokenize(tweet) if w.lower() in words or not w.isalpha())
+    tweet = " ".join(w for w in nltk.wordpunct_tokenize(tweet) if w.lower() in tweet or not w.isalpha())
     return tweet
+
+
+def load_model_tokenizer(model_path):
+    return BertTokenizerFast.from_pretrained(model_path), \
+           BertForSequenceClassification.from_pretrained(model_path, num_labels=8).to("cpu")
 
 
 class Guesser:
     def __init__(self):
-        self.dom_model_path = "know_your_witter/typer/models/16personalities-dominant-function-bert-small-uncased"
-        self.sec_model_path = ""
+        self.dom_model_path = "models/16personalities-dominant-function-bert-small-uncased"
+        self.sec_model_path = "models/16personalities-secondary-function-bert-small-uncased"
         self.tweets = []
-        self.dom_tokenizer, self.dom_model = self.load_model_tokenizer(self.dom_model_path)
-        self.sec_tokenizer, self.sec_model = self.load_model_tokenizer(self.sec_model_path)
+        self.dom_tokenizer, self.dom_model = load_model_tokenizer(self.dom_model_path)
+        self.sec_tokenizer, self.sec_model = load_model_tokenizer(self.sec_model_path)
         self.max_length = 512
+        self.predicted = self.predict_type()
 
     def prepare_tweets(self, tweets):
         for tweet in tweets:
             self.tweets.append(clean_tweet(tweet))
 
-    def load_model_tokenizer(self, model_path):
-        return BertTokenizerFast.from_pretrained(model_path), \
-               BertForSequenceClassification.from_pretrained(model_path, num_labels=8).to("cpu")
-
-    def get_dom_prediction(self, text):
-        inputs = self.dom_tokenizer(text, padding=True, truncation=True, max_length=self.max_length,
+    def get_dom_prediction(self):
+        inputs = self.dom_tokenizer(' '.join(self.tweets), padding=True, truncation=True, max_length=self.max_length,
                                     return_tensors="pt").to("cpu")
         outputs = self.dom_model(**inputs)
         probs = outputs[0].softmax(1)
         return probs.argmax().item()
 
-    def get_sec_prediction(self, text):
-        inputs = self.sec_tokenizer(text, padding=True, truncation=True, max_length=self.max_length,
+    def get_sec_prediction(self):
+        inputs = self.sec_tokenizer(' '.join(self.tweets), padding=True, truncation=True, max_length=self.max_length,
                                     return_tensors="pt").to("cpu")
         outputs = self.sec_model(**inputs)
         probs = outputs[0].softmax(1)
-        return probs.argmax().item()
+        return probs
 
-    def decode_prediction(self):
-
-
-    def return_predicted(self):
-        return
+    def predict_type(self):
+        primary_function = self.get_dom_prediction()
+        secondary = self.get_sec_prediction()
+        if primary_function == 0:
+            if secondary.item()[5] > secondary.item()[7]:
+                return "ESFP"
+            return "ESTP"
+        elif primary_function == 1:
+            if secondary.item()[4] > secondary.item()[6]:
+                return "ISFJ"
+            return "ISTJ"
+        elif primary_function == 2:
+            if secondary.item()[5] > secondary.item()[7]:
+                return "ENFP"
+            return "ENTP"
+        elif primary_function == 3:
+            if secondary.item()[4] > secondary.item()[6]:
+                return "INFJ"
+            return "INTJ"
+        elif primary_function == 4:
+            if secondary.item()[3] > secondary.item()[1]:
+                return "ENFJ"
+            return "ESFJ"
+        elif primary_function == 5:
+            if secondary.item()[0] > secondary.item()[2]:
+                return "ISFP"
+            return "INFP"
+        elif primary_function == 6:
+            if secondary.item()[3] > secondary.item()[1]:
+                return "ENTJ"
+            return "ESTJ"
+        elif primary_function == 7:
+            if secondary.item()[0] > secondary.item()[2]:
+                return "ISTP"
+            return "INTP"
 
 
 def guess_personality(username):
-    #tweets = api.get_tweets(username)
-    tweets = ['']
+    tweets = api.get_tweets(username)
     guesser = Guesser()
     guesser.prepare_tweets(tweets)
-    return "INTP"
+    return guesser.predicted
 
 
 if __name__ == '__main__':
